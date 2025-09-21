4e8163b71bce716799daa180239211a284436c8f
niladri.mallik007@gmail.com <51795733+NiladriMallik@users.noreply.github.com>
2025-09-21 23:54:41 +0530

    NamasteSQL: 61 & 62 - Flipkart Category Sales (Part 1 & 2)

--- NamasteSQL/61. Flipkart Category Sales (Part 1).py
+++ NamasteSQL/61. Flipkart Category Sales (Part 1).py
@@ -1,12 +1,14 @@
 from pyspark.sql import functions as F
 
-sales_df.groupBy('category').agg(
-    F.sum(F.col('amount')).alias('SumOfSales')
-    )\
+sales_df\
     .filter(
         (F.col('order_date').like('2022-02%')) &
         (
-            (F.format(F.col('order_date'), 'EEEE') != 'Saturday') &
-            (F.format(F.col('order_date'), 'EEEE') != 'Sunday')
+            (F.date_format(F.col('order_date'), 'EEEE') != 'Saturday') &
+            (F.date_format(F.col('order_date'), 'EEEE') != 'Sunday')
         )
-).show()
+)\
+    .groupBy('category')\
+        .agg(F.sum('amount').alias('total_sales'))\
+            .sort(F.col('total_sales').asc())\
+                .show()
