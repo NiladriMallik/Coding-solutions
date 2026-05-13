fe826b9fd15e2b8addf73df25bb978f7040938ba
Niladri Mallik <51795733+NiladriMallik@users.noreply.github.com>
2026-05-13 19:56:41 +0530

    StrataScratch: 9817 PySpark

---
+++ StrataScratch/9817 - Count Occurrences Of Words In Drafts.py
@@ -0,0 +1,13 @@
+# Import your libraries
+import pyspark
+from pyspark.sql import functions as F
+
+# Start writing code
+output = google_file_store.withColumn(
+    'words', F.explode(F.split(F.col('contents'), ' '))
+).groupBy('words').agg(
+    F.count("*").alias("occurrences")
+).orderBy("occurrences", ascending = False)
+
+# To validate your solution, convert your final pySpark df to a pandas df
+output.toPandas()
